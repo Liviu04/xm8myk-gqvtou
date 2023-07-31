d111851b63acb7aadbb092cbeb344ea730f1423f
Liviu04 <72658849+Liviu04@users.noreply.github.com>
2023-07-31 17:47:58 +0300

    Initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product : Product | undefined;
+  constructor(private route: ActivatedRoute){ }
+
+  ngOnInit(){
+
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'))
+  
+    this.product = products.find(product => product.id == productIdFromRoute);
+  }
+
+}
+
+
+
+
